676d3563e16d992120ac08fa20eb962c2435d837
Filip Palusiński <palusinskifilip@gmail.com>
2021-09-28 20:10:03 +0200

    Completed 3th step

--- app/src/main/java/pl/studioandroida/layoutsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/pl/studioandroida/layoutsinjetpackcompose/MainActivity.kt
@@ -3,14 +3,20 @@ package pl.studioandroida.layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,8 +34,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotographerCard() {
-    Row{
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row (modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /*ingore clicking */ })
+        .padding(16.dp)
+    ){
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
@@ -37,12 +49,17 @@ fun PhotographerCard() {
         ) {
             //Image here
         }
+        Column (
+            modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+                ){
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
     }
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
-        }
+
     }
 }
 
